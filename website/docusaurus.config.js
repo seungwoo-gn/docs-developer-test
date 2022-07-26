ed1889b7bf151f8f9c3addff1587dd79d440203f
daeho-gn <daeho.kim@genesisnest.com>
2022-07-26 21:06:59 +0900

    Modify docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'genesisnest', // Usually your GitHub org/user name.
-    projectName: 'docs-developer', // Usually your repo name.
+    projectName: 'docs-developer-test', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
